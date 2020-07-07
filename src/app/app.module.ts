a78147e4a163b11a647f5e020402f044bbed2faf
Kainat Aijaz <kainatajz2021@gmail.com>
2020-07-08 00:04:37 +0500

    Single Page Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,24 +12,42 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service'
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailsComponent
+    DishdetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,15 @@ import { Injectable } from '@angular/core';
 export class DishService {
 
   constructor() { }
+
+  getDishes(): Dish[] {
+    return DISHES;
+  }
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
+  }
 }
